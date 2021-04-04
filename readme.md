fd72df0afe196e43768fc36f6f60dc71c7f5d6e9
unknown <alia.azarian@gmail.com>
2021-04-04 23:50:06 +0100

    readme updated

--- readme.md
+++ readme.md
@@ -1,16 +1,1 @@
-# Three.js Journey
-
-## Setup
-Download [Node.js](https://nodejs.org/en/download/).
-Run this followed commands:
-
-``` bash
-# Install dependencies (only the first time)
-npm install
-
-# Run the local server at localhost:8080
-npm run dev
-
-# Build for production in the dist/ directory
-npm run build
-```
+![particles](https://user-images.githubusercontent.com/63723832/113523600-682cbb00-95a0-11eb-9bf3-c96de577d709.jpg)
--- src/script.js
+++ src/script.js
@@ -19,15 +19,48 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('textures/particles/2.png')
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+// Particles
+const particlesGeometry = new THREE.BufferGeometry(1, 32, 32)
+const count = 5000
+
+const positions = new Float32Array(count * 3)   // XYZ
+const colors = new Float32Array(count * 3)      // RGB
+
+for(let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
 )
-scene.add(cube)
+
+particlesGeometry.setAttribute(
+    'color',
+    new THREE.BufferAttribute(colors, 3)
+)
+
+// console.log(particlesGeometry.attributes.position.array)
+
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,           // close to camera, particle larger
+    // color: '#ff88cc'
+})
+particlesMaterial.transparent = true
+particlesMaterial.alphaMap = particleTexture
+// particlesMaterial.alphaTest = 0.001
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.vertexColors = true
+
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
@@ -81,6 +114,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Particles
+    // particles.rotation.y = elapsedTime * 0.2   
+    // particles.position.y = -elapsedTime * 0.2
+    for(let i = 0; i < count; i++) {
+        const i3 = i * 3       // or "i < count * 3"
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+    }
+    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
